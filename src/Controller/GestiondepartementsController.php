be4327eb1a36aefd6ea03bd76e323a437a7d536e
unknown <abdelkebir.fedi@esprit.tn>
2022-08-06 11:59:20 +0100

    gestion departement crud

---
+++ src/Controller/GestiondepartementsController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Department;
+use App\Form\DepartementType;
+class GestiondepartementsController extends AbstractController
+{
+    #[Route('/Departements', name: 'app_gestiondepartements')]
+    public function index(): Response
+    {
+        $repo=$this->getDoctrine()->getRepository(Department::class);
+        $DepartementsList=$repo->findAll();
+        return $this->render('gestiondepartements/index.html.twig', [
+            'controller_name' => 'GestiondepartementsController',
+            'DepartementsList'=>$DepartementsList,
+        ]);
+    }
+    //Ajouter
+    #[Route('/Departements/ajouterdepartement', name: 'AjouterDepartement')]
+    public function AjouterDepartement(Request $req): Response
+    {
+        $idcreator=1;
+        $Departements = new Department();
+        $form = $this->createForm(DepartementType::class, $Departements);
+        $form->handleRequest($req);
+        if($form->isSubmitted() && $form->isValid()){
+            $this->getDoctrine()->getManager()->persist($Departements);
+            $this->getDoctrine()->getManager()->flush();   
+            
+            return $this->redirectToRoute('app_gestiondepartements');
+        }
+        return $this->render('gestiondepartements/ajouterdepartement.html.twig', [
+            'controller_name' => 'GestiondepartementController',
+            'formajoutdepartement' => $form->createView()
+        ]);
+    }
+    //Modifier
+    #[Route('/Departements/modifierdepartement/{id}', name: 'ModifierDepartement')]
+    public function ModifierDepartement(Department $Departement, Request $req,int $id)
+{   
+        $formedit = $this->createForm(DepartementType::class, $Departement);
+        $formedit->handleRequest($req);
+        if($formedit->isSubmitted() && $formedit->isValid()){
+            $this->getDoctrine()->getManager()->persist($Departement); 
+            $this->getDoctrine()->getManager()->flush(); 
+              
+        }
+        return $this->render('gestiondepartements/modifierdepartement.html.twig', [
+            'controller_name' => 'GestionDepartementController',
+            'formeditdepartement' => $formedit->createView(),
+            'id' => $Departement->getId()
+        ]);
+    }
+    //Supprimer
+    #[Route('/Departements/delete/{id}', name: 'deletedepartement')]
+    public function delete(Request $request, $id): Response
+    { $em = $this->getDoctrine()->getManager();
+        $Departement = $em
+            ->getRepository(Department::class)
+            ->find($id);
+        $em->remove($Departement);
+        $em->flush();
+        return $this->redirectToRoute('app_gestiondepartements');
+    }
+}
---
+++ src/Form/DepartementType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Department;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DepartementType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('libelle',null,['label' => false])
+            ->add('dataAjout',null,['label' => false])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Department::class,
+        ]);
+    }
+}
