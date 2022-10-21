9bc18948b8d4cf6b5d57a5a36430958413f4b959
unknown <abdelkebir.fedi@esprit.tn>
2022-10-21 12:50:12 +0100

    Gestion Voyage Fix

--- src/Controller/GestionvoyagesController.php
+++ src/Controller/GestionvoyagesController.php
@@ -9,6 +9,8 @@ use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\VoyageEffectuer;
 use App\Form\VoyageEffectuerType;
+use App\Entity\Chauffeur;
+
 class GestionvoyagesController extends AbstractController
 {
     #[Route('/Voyages', name: 'app_gestionvoyages')]
@@ -16,6 +18,7 @@ class GestionvoyagesController extends AbstractController
     {
         $repo=$this->getDoctrine()->getRepository(VoyageEffectuer::class);
         $VoyagesList=$repo->findAll();
+        dump($VoyagesList);
         return $this->render('gestionvoyages/index.html.twig', [
             'controller_name' => 'GestionvoyagesController',
             'VoyagesList'=>$VoyagesList,
@@ -24,12 +27,19 @@ class GestionvoyagesController extends AbstractController
     //Ajouter
     #[Route('/Voyages/ajoutervoyage', name: 'AjouterVoyage')]
     public function AjouterVoyage(Request $req): Response
-    {
+    {   
+        $repochauffeur=$this->getDoctrine()->getRepository(Chauffeur::class);
+        $ChauffeurList=$repochauffeur->findAll();
+        //
         $idcreator=1;
         $Voyages = new VoyageEffectuer();
         $form = $this->createForm(VoyageEffectuerType::class, $Voyages);
         $form->handleRequest($req);
         if($form->isSubmitted() && $form->isValid()){
+            $idchauffeur=$req->request->get('chauffeur');
+            $chauffeur=$repochauffeur->find((int)$idchauffeur);
+            //dd($chauffeur);
+            $Voyages->setChauffeur($chauffeur);
             $this->getDoctrine()->getManager()->persist($Voyages);
             $this->getDoctrine()->getManager()->flush();   
             
@@ -37,28 +47,43 @@ class GestionvoyagesController extends AbstractController
         }
         return $this->render('gestionvoyages/ajoutervoyage.html.twig', [
             'controller_name' => 'GestionvoyageController',
-            'formajoutvoyage' => $form->createView()
+            'formajoutvoyage' => $form->createView(),
+            'ChauffeurList'=>$ChauffeurList
         ]);
     }
     //Modifier
     #[Route('/Voyages/modifiervoyage/{id}', name: 'ModifierVoyage')]
     public function ModifierVoyage(VoyageEffectuer $Voyage, Request $req,int $id)
-{   
+{       $oldchauffeur=$Voyage->getChauffeur();
+        $repochauffeur=$this->getDoctrine()->getRepository(Chauffeur::class);
+        $ChauffeurList=$repochauffeur->findAll();
+        //
         $formedit = $this->createForm(VoyageEffectuerType::class, $Voyage);
         $formedit->handleRequest($req);
         if($formedit->isSubmitted() && $formedit->isValid()){
+            if($Voyage->getChauffeur()==null){
+                $Voyage->setChauffeur($oldchauffeur);
+            }
+            else{
+                $idchauffeur=$req->request->get('chauffeur');
+                $chauffeur=$repochauffeur->find((int)$idchauffeur);
+                $Voyage->setChauffeur($chauffeur);
+            }
+            //dd($Voyage);
             $this->getDoctrine()->getManager()->persist($Voyage); 
             $this->getDoctrine()->getManager()->flush(); 
+            return $this->redirectToRoute('app_gestionvoyages');
               
         }
         return $this->render('gestionvoyages/modifiervoyage.html.twig', [
             'controller_name' => 'GestionVoyageController',
             'formeditvoyage' => $formedit->createView(),
-            'id' => $Voyage->getId()
+            'id' => $Voyage->getId(),
+            'ChauffeurList'=>$ChauffeurList
         ]);
     }
     //Supvoyager
-    #[Route('/Voyages/delete/{id}', name: 'deletevoyage')]
+    #[Route('/Voyages/deletevoyage/{id}', name: 'deletevoyage')]
     public function delete(Request $request, $id): Response
     { $em = $this->getDoctrine()->getManager();
         $Voyage = $em
--- src/Form/VoyageEffectuerType.php
+++ src/Form/VoyageEffectuerType.php
@@ -6,17 +6,16 @@ use App\Entity\VoyageEffectuer;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 class VoyageEffectuerType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('pointDebut',null,['label' => false])
-            ->add('dateDebut',null,['label' => false])
+            ->add('dateDebut',DateType::class,['label' => false])
             ->add('pointFinale',null,['label' => false])
-            ->add('dateFinale',null,['label' => false])
-            ->add('Chauffeur',null,['label' => false])
+            ->add('dateFinale',DateType::class,['label' => false])
         ;
     }
 
